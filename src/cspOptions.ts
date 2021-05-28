f06e43634add318c2b6b3e665aeab8d3d05cd06d
Cloudless67 <cloudlesslakitu67@gmail.com>
2021-05-28 20:49:44 +0900

    Updated CSP options to allow google fonts

--- src/cspOptions.ts
+++ src/cspOptions.ts
@@ -3,13 +3,19 @@ export default {
         defaultSrc: ["'self'"],
         imgSrc: ['*', 'data:'],
         scriptSrcElem: ["'self'", 'https://kit.fontawesome.com/bb93986db9.js'],
-        styleSrc: ["'self'", "'unsafe-inline'"],
+        styleSrc: ["'self'", "'unsafe-inline'", 'fonts.googleapis.com'],
         styleSrcElem: [
             "'self'",
             "'unsafe-inline'",
             'https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css',
+            'fonts.googleapis.com',
         ],
-        fontSrc: ["'self'", 'https://cdn.jsdelivr.net/', 'https://ka-f.fontawesome.com/'],
+        fontSrc: [
+            "'self'",
+            'https://cdn.jsdelivr.net/',
+            'https://ka-f.fontawesome.com/',
+            'fonts.gstatic.com',
+        ],
         connectSrc: ["'self'", 'https://ka-f.fontawesome.com'],
     },
 };
