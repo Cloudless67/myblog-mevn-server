84ac583e886bea10c77a22df7974ea1a6bf437a1
Cloudless67 <cloudlesslakitu67@gmail.com>
2021-11-25 16:00:15 +0900

    [#2] Removed @types/mongoose

--- src/controllers/Authentication.test.ts
+++ src/controllers/Authentication.test.ts
@@ -1,7 +1,5 @@
 import { verifyPassword } from './Authentication';
 
-const RAW_PASSWORD = process.env.RAW_PASSWORD || '';
-
 describe('Authentication', () => {
     it('should return 403 for wrong password', async () => {
         const { status, payload } = await verifyPassword('Wrong password');
